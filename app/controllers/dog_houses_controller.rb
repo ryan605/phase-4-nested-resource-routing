8d1b7e6845b2361523a8b5b0a2953be3ee15e76e
ryan605 <langatryan26@gmail.com>
2023-03-22 14:19:50 +0300

    done

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -12,4 +12,13 @@ class DogHousesController < ApplicationController
     render json: { error: "Dog house not found" }, status: :not_found
   end
 
+  def index
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+    render json: reviews, include: :dog_house
+  end
 end
